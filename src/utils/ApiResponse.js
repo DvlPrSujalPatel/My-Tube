4f8af4eb4b859e36d94c2b6b69563eeb69693a41
DvlPrSujalPatel <sujal7455@gmail.com>
2024-08-23 19:00:58 +0530

    utils code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.data = data
+    this.message = message
+    this.success = statusCode
+    this.statusCode = statusCode < 400
+  }
+}
+
+export default ApiResponse
